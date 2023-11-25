591b913eddfdd172d837a2935c5fd3f1e4f1eca0
Nicolas Colli <115481126+nColli@users.noreply.github.com>
2023-11-25 13:14:01 -0300

    constructor ConvocatoriaNoJerarquico mas legible
    Co-authored-by: Nahuel Mosse <nmosse@kovix-group.com>

--- ParcialProgramacion4/src/ConvocatoriaNoJerarquico.java
+++ ParcialProgramacion4/src/ConvocatoriaNoJerarquico.java
@@ -3,7 +3,13 @@ import java.util.Hashtable;
 import utilidades.Fecha;
 
 public class ConvocatoriaNoJerarquico extends Convocatoria {
-    public ConvocatoriaNoJerarquico(int codigo, Puesto puesto, Fecha fecha, int cantEmpleadosRequeridos, Hashtable<Habilidad,Integer> requisitos) {
+    public ConvocatoriaNoJerarquico(
+        int codigo,
+        Puesto puesto,
+        Fecha fecha,
+        int cantEmpleadosRequeridos,
+        Hashtable<Habilidad,Integer> requisitos
+    ) {
         super(codigo, puesto, fecha, cantEmpleadosRequeridos, requisitos);
     }
 }
