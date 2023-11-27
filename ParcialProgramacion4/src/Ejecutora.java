e7be76d41624aec401d14f636fc08642f0ce3e09
Nahuel Mosse <nmosse@kovix-group.com>
2023-11-26 21:00:07 -0300

    Update Ejecutora.java

--- ParcialProgramacion4/src/Ejecutora.java
+++ ParcialProgramacion4/src/Ejecutora.java
@@ -28,15 +28,15 @@ public class Ejecutora {
     	Logger.divider();
     }
 	
-	public static void menuPrincipal(Empresa empresa, Scanner scanner) {
-		int opcion = 0;
+    public static void menuPrincipal(Empresa empresa, Scanner scanner) {
+	int opcion = 0;
 		
-        do{
-        	Logger.divider();
-        	System.out.println("Menu Principal");
-        	Logger.divider();
+        do {
+            Logger.divider();
+            System.out.println("Menu Principal");
+            Logger.divider();
         	
-        	System.out.println("\nSeleccione una de las opciones de la lista");
+            System.out.println("\nSeleccione una de las opciones de la lista");
             System.out.println("\n[1] Admin");
             System.out.println("[2] General");
             System.out.println("[3] Usuario");
@@ -46,39 +46,39 @@ public class Ejecutora {
             opcion = Ejecutora.conseguirOpcionDelMenu(scanner);
 
             switch (opcion) {
-	            case 0:
-	            	// Salir
-	                break;
-	            case 1:
-	            	Ejecutora.menuAdmin(empresa, scanner);
-	                break;
-	            case 2:
-	            	Ejecutora.menuGeneral(empresa, scanner);
-	                break;
-	            case 3:
-	            	Ejecutora.menuUsuario(empresa, scanner);
-	                break;
-	            case 4:
-	                // ayuda
-	            	// Se puede hacer todo un menu que te explique a usar el sistema
-	                break;
-	            default:
-	            	Logger.logError("La opcion " + opcion + " no esta en la lista");
-	                break;
+	        case 0:
+	            // Salir
+	            break;
+	        case 1:
+	            Ejecutora.menuAdmin(empresa, scanner);
+	            break;
+	        case 2:
+	            Ejecutora.menuGeneral(empresa, scanner);
+	            break;
+	        case 3:
+	            Ejecutora.menuUsuario(empresa, scanner);
+	            break;
+	        case 4:
+	            // ayuda
+	            // Se puede hacer todo un menu que te explique a usar el sistema
+	            break;
+	        default:
+	            Logger.logError("La opcion " + opcion + " no esta en la lista");
+	            break;
             }
             
         } while(opcion != 0);
-	}
+    }
 	
-	public static void menuAdmin(Empresa empresa, Scanner scanner) {
-		int opcion = 0;
+    public static void menuAdmin(Empresa empresa, Scanner scanner) {
+	int opcion = 0;
 		
-        do{
-        	Logger.divider();
-        	System.out.println("Menu Admin");
-        	Logger.divider();
+        do {
+            Logger.divider();
+            System.out.println("Menu Admin");
+            Logger.divider();
         	
-        	System.out.println("\nSeleccione una de las opciones de la lista");
+            System.out.println("\nSeleccione una de las opciones de la lista");
             System.out.println("\n[1] Agregar empleado");
             System.out.println("[2] Dar de baja empleado");
             System.out.println("[3] Agregar puesto de trabajo");
@@ -91,47 +91,47 @@ public class Ejecutora {
             opcion = Ejecutora.conseguirOpcionDelMenu(scanner);
 
             switch (opcion) {
-	            case 0:
-	            	// Volver al menu principal
-	                break;
-	            case 1:
-	            	// empresa.agregarEmpleado();
-	                break;
-	            case 2:
+	        case 0:
+	            // Volver al menu principal
+	            break;
+	        case 1:
+	            // empresa.agregarEmpleado();
+	            break;
+	        case 2:
 	            	
-	                break;
-	            case 3:
+	            break;
+	        case 3:
 	                
-	                break;
-	            case 4:
+	            break;
+	        case 4:
 	            	
-	                break;
-	            case 5:
-	            	// empresa.agregarHabilidad();
-	                break;
-	            case 6:
+	            break;
+	        case 5:
+	            // empresa.agregarHabilidad();
+	            break;
+	        case 6:
 	            	
-	                break;
-	            case 7:
+	            break;
+	        case 7:
 	            	
-	                break;
-	            default:
-	            	Logger.logError("La opcion " + opcion + " no esta en la lista");
-	                break;
+	            break;
+	        default:
+	            Logger.logError("La opcion " + opcion + " no esta en la lista");
+	            break;
             }
             
         } while(opcion != 0);
-	}
+    }
 	
-	public static void menuGeneral(Empresa empresa, Scanner scanner) {
-		int opcion = 0;
+    public static void menuGeneral(Empresa empresa, Scanner scanner) {
+	int opcion = 0;
 		
-        do{
-        	Logger.divider();
-        	System.out.println("Menu General");
-        	Logger.divider();
+        do {
+            Logger.divider();
+            System.out.println("Menu General");
+            Logger.divider();
         	
-        	System.out.println("\nSeleccione una de las opciones de la lista");
+            System.out.println("\nSeleccione una de las opciones de la lista");
             System.out.println("\n[1] Crear una nueva convocatoria");
             System.out.println("[2] Editar convocatoria");
             System.out.println("[3] Dar de baja convocatoria");
@@ -146,55 +146,55 @@ public class Ejecutora {
             opcion = Ejecutora.conseguirOpcionDelMenu(scanner);
 
             switch (opcion) {
-	            case 0:
-	            	// Volver al menu principal
-	                break;
-	            case 1:
-	            	// empresa.agregarConvocatoria();
-	            	// empresa.agregarPuestoVacante(); // esto deberia ser crear convocatoria
-	            	// Revisar como fusionar estos dos en uno
-	                break;
-	            case 2:
+	        case 0:
+	            // Volver al menu principal
+	            break;
+	        case 1:
+	            // empresa.agregarConvocatoria();
+	            // empresa.agregarPuestoVacante(); // esto deberia ser crear convocatoria
+	            // Revisar como fusionar estos dos en uno
+	            break;
+	        case 2:
 	                
-	                break;
-	            case 3:
+	            break;
+	        case 3:
 	            	
-	                break;
-	            case 4:
+	            break;
+	        case 4:
 	                
-	                break;
-	            case 5:
+	            break;
+	        case 5:
 	                
-	                break;
-	            case 6:
+	            break;
+	        case 6:
 	            	
-	                break;
-	            case 7:
-	            	// empresa.inscribirEmpleadoConvocatoria();
-	                break;
-	            case 8:
+	            break;
+	        case 7:
+	            // empresa.inscribirEmpleadoConvocatoria();
+	            break;
+	        case 8:
 	            	
-	                break;
-	            case 9:
+	            break;
+	        case 9:
 	            	
-	                break;
-	            default:
-	            	Logger.logError("La opcion " + opcion + " no esta en la lista");
-	                break;
+	            break;
+	        default:
+	            Logger.logError("La opcion " + opcion + " no esta en la lista");
+	            break;
             }
             
         } while(opcion != 0);
-	}
+    }
 	
-	public static void menuUsuario(Empresa empresa, Scanner scanner) {
-		int opcion = 0;
+    public static void menuUsuario(Empresa empresa, Scanner scanner) {
+	int opcion = 0;
 		
-        do{
-        	Logger.divider();
-        	System.out.println("Menu Usuario");
-        	Logger.divider();
+        do {
+            Logger.divider();
+            System.out.println("Menu Usuario");
+            Logger.divider();
         	
-        	System.out.println("\nSeleccione una de las opciones de la lista");
+            System.out.println("\nSeleccione una de las opciones de la lista");
             System.out.println("\n[1] Ver datos de empleado");
             System.out.println("[2] Ver historial de cargos");
             System.out.println("[3] Editar datos");
@@ -203,54 +203,54 @@ public class Ejecutora {
             System.out.println("[6] Quitar habilidad");
             System.out.println("[0] Volver al menu principal");
             
-            opcion = Ejecutora.conseguirOpcionDelMenu(scanner);
+    	    opcion = Ejecutora.conseguirOpcionDelMenu(scanner);
 
             switch (opcion) {
-	            case 0:
-	            	// Volver al menu principal
-	                break;
-	            case 1:
+	        case 0:
+	            // Volver al menu principal
+	            break;
+	        case 1:
 	            	
-	                break;
-	            case 2:
+	            break;
+	        case 2:
 	                
-	                break;
-	            case 3:
+	            break;
+	        case 3:
 	                
-	                break;
-	            case 4:
+	            break;
+	        case 4:
 	                
-	                break;
-	            case 5:
+	            break;
+	        case 5:
 	                
-	                break;
-	            case 6:
+	            break;
+	        case 6:
 	            	
-	                break;
-	            default:
-	            	Logger.logError("La opcion " + opcion + " no esta en la lista");
-	                break;
+	            break;
+	        default:
+	            Logger.logError("La opcion " + opcion + " no esta en la lista");
+	            break;
             }
             
         } while(opcion != 0);
-	}
+    }
 	
-	public static int conseguirOpcionDelMenu(Scanner scanner) {
-		int opcion = 0;
+    public static int conseguirOpcionDelMenu(Scanner scanner) {
+	int opcion = 0;
 	
         boolean huboError = false;
-		do {
-			huboError = false;
-			// Evita que se cuelgue el sistema cuando se ingresa un string
-			try {
-				System.out.print("\nopcion: ");
-				opcion = Integer.parseInt(scanner.nextLine());
-			} catch (Exception _error) {
-				huboError = true;
-				Logger.logError("Solo estan permitidos caracteres numericos");
-			}
-		} while (huboError);
+	do {
+	    huboError = false;
+	    // Evita que se cuelgue el sistema cuando se ingresa un string
+	    try {
+		System.out.print("\nopcion: ");
+		opcion = Integer.parseInt(scanner.nextLine());
+	    } catch (Exception _error) {
+		huboError = true;
+		Logger.logError("Solo estan permitidos caracteres numericos");
+	    }
+	} while (huboError);
 		
-		return opcion;
-	}
+	return opcion;
+    }
 }
