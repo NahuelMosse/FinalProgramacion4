41e9f919296141db9964f4178ed51cd3149b5ff6
Nahuel Mosse <nmosse@kovix-group.com>
2023-11-27 18:01:07 -0300

    CU crear habilidad (#13)
    * CU crear habilidad
    
    * fix

--- ParcialProgramacion4/src/Habilidad.java
+++ ParcialProgramacion4/src/Habilidad.java
@@ -1,11 +1,17 @@
 public class Habilidad {
-    private int codigo;
     private String nombre;
     private String descripcion;
 
-    public Habilidad(int codigo, String nombre, String descripcion) {
-        this.codigo = codigo;
+    public Habilidad(String nombre, String descripcion) {
         this.nombre = nombre;
         this.descripcion = descripcion;
+    }
+    
+    public boolean hasName(String nombre) {
+    	// Buscamos sin espacion para encontrar mas similitudes
+    	String nombreHabilidadSinEspacios = this.nombre.replace(" ", "");
+    	String nombreABuscarSinEspacios = nombre.replace(" ", ""); 
+    	
+        return nombreHabilidadSinEspacios.equalsIgnoreCase(nombreABuscarSinEspacios);
     }
 }
