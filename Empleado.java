ab94935d2905e0cf88f0cb769770dbca81b2a288
Hachotek <110694568+hachotek@users.noreply.github.com>
2023-11-27 22:11:52 -0300

    cu7 - AGREGAR HABILIDAD DESDE EL EMPLEADO
    la idea de este caso es desde el menu del empleado acceder a la empresa , desde la empresa busca el empleado , busca la habilidad si no la encuentra llama el metodo de nahue para agregarla si la encuentra pide los anios y le dice al empleado que la agrege a su hashtable

---
+++ Empleado.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import utilidades.Fecha;
+import java.util.scanner;
+
+public class Empleado {
+
+    Scanner scanner = new Scanner(System.in);
+
+    private int legajo;
+    private String nombre;
+    private String apellido;
+    private Fecha fechaNacimiento;
+    private ArrayList<Cargo>historialDeCargos;
+    private Hashtable<Habilidad,Integer> habilidades;
+    private Fecha fechaDeIngreso;
+
+    public Empleado(
+        int legajo,
+        String nombre,
+        String apellido,
+        Fecha fechaNacimiento,
+        Fecha fechaDeIngreso, ArrayList<Cargo> historialDeCargos,
+        Hashtable<Habilidad,Integer> habilidades
+    ) {
+        this.legajo = legajo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.historialDeCargos = historialDeCargos;
+        this.habilidades = habilidades;
+        this.fechaDeIngreso = fechaDeIngreso;
+    }
+    
+
+    public void setNombre(String nombre) {
+    	this.nombre = nombre;
+    }
+    
+    public void setApellido(String apellido) {
+    	this.apellido = apellido;
+    }
+    
+    public void setFechaNacimiento(Fecha fechaNacimiento) {
+    	this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    public void setFechaIngreso(Fecha fechaIngreso) {
+    	this.fechaDeIngreso = fechaIngreso;
+    }
+    
+
+    //SI MAL NO RECUERDO ESTO RECIBE LA CLASE Y UN ENTERO QUE LO EMPAQUETA EN UN INTEGER/.
+    public void agregarHabilidadEmpleado(Habilidad habilidadExistente,int unaCantidadDeTiempo)
+    {
+        if(habilidades.containsKey(habilidadExistente))  // SI CONTIENE LA POS
+			aniosExp+=habilidades.get(habilidadExistente); // AL ENC LE PONGO LOS ANIOS???
+		habilidades.put(habilidadExistente,aniosExp);
+    }
+    
+}
