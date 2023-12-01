3f4867a21e83657e62dc5afc5a16df90fb2a13d1
Nicolas Colli <115481126+nColli@users.noreply.github.com>
2023-11-30 23:41:09 -0300

    Cu editar habilidad menu general (NO del empleado) (#36)
    * editar habilidad con menu
    
    * mejora menu
    
    * intento evitar conflicto
    
    * intento solucion conflicto
    
    * duplicado borrado
    
    * Update ParcialProgramacion4/src/Empresa.java
    
    Co-authored-by: Nahuel Mosse <nmosse@kovix-group.com>
    
    * metodos individuales
    
    ---------
    
    Co-authored-by: Nahuel Mosse <nmosse@kovix-group.com>

--- ParcialProgramacion4/src/Habilidad.java
+++ ParcialProgramacion4/src/Habilidad.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+
+import utilidades.Logger;
+
 public class Habilidad {
     private String nombre;
     private String descripcion;
@@ -23,5 +27,33 @@ public class Habilidad {
 
     public String getNombre() {
         return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+
+    public void editarNombre(Scanner scanner) {
+        System.out.println("Nombre antiguo: " + nombre);
+        System.out.print("Nuevo nombre: ");
+        nombre = scanner.nextLine();
+        Logger.logSuccess("Nombre editado correctamente");
+    }
+
+    public void editarDescripcion(Scanner scanner) {
+        System.out.println("Descripcion antigua: ");
+        System.out.println(descripcion);
+        System.out.print("Nueva descripcion: ");
+        descripcion = scanner.nextLine();
+        Logger.logSuccess("Descripcion editada correctamente");
     }
 }
