3b8558e264d8dbee4fdc2940c7934ac79fd47618
Nicolas Colli <collinicolas01@gmail.com>
2023-11-30 22:27:42 -0300

    duplicado borrado

--- ParcialProgramacion4/src/Habilidad.java
+++ ParcialProgramacion4/src/Habilidad.java
@@ -25,11 +25,6 @@ public class Habilidad {
         return this.nombre;
     }
 
-
-    public String getNombre() {
-        return nombre;
-    }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
