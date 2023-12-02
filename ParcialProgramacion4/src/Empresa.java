ad7507725b4074460c198093240f8733d540db8a
Nicolas Colli <collinicolas01@gmail.com>
2023-12-01 22:55:18 -0300

    ultimo cambio por favorrrr

--- ParcialProgramacion4/src/Empresa.java
+++ ParcialProgramacion4/src/Empresa.java
@@ -1172,9 +1172,9 @@ public class Empresa {
                 System.out.println("[1] Ver todas las convocatorias a las que puede aplicar");
                 System.out.println("[2] Ver convocatorias de un puesto especifico");
                 System.out.println("[3] Ver convocatorias para un rango de salario");
-                System.out.println("[0] Volver");
+                System.out.println("[0] Volver al menu General");
 
-                opcion = InputHelper.scanInt(scanner, "Opcion: ");
+                opcion = InputHelper.scanInt(scanner, "\nOpcion: ");
 
                 switch (opcion) {
                     case 0:
